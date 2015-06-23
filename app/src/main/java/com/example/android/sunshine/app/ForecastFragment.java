2d23d3edc90daa08f028d9ccc79746607454f0f8
a580536 <erik-jan.krielen@atos.net>
2015-06-23 10:54:02 +0200

    ForecastFragment now uses preference for loction

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -2,9 +2,11 @@ package com.example.android.sunshine.app;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -68,8 +70,10 @@ public class ForecastFragment extends Fragment {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_refresh) {
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String userLocation = sharedPref.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("Utrecht,NL");
+            weatherTask.execute(userLocation);
             return true;
         }
 
